9b66c90bc21eaa2c109dde1be5abd4a385dc9a43
Johan Andersson von Geijer <Johan.Andersson.von.Geijer@ub.gu.se>
2015-02-18 23:49:40 +0100

    DFLOW-90 Towards a functioning implementation of create jobs. Among other things the method is now named create, and the tab signs are all replaced by two spaces per tab.

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -1,12 +1,12 @@
 
 class Api::JobsController < Api::ApiController
-	before_filter :check_key
-	before_filter :check_params
+  before_filter :check_key
+  before_filter :check_params
 
 
-	# Returns all jobs
-	def index
-		jobs = Job.all 
+  # Returns all jobs
+  def index
+    jobs = Job.all
     pagination = {}
     if !jobs.empty?
       tmp = jobs.paginate(page: params[:page])
@@ -36,7 +36,7 @@ class Api::JobsController < Api::ApiController
     @response[:meta] = {query: metaquery, pagination: pagination}
 
     render_json
-	end
+  end
 
   def show
     begin
@@ -45,141 +45,171 @@ class Api::JobsController < Api::ApiController
     rescue
       error_msg(ErrorCodes::REQUEST_ERROR, "Could not find job '#{params[:id]}'")
     end
-		render_json
+    render_json
   end
 
-	# Returns the metadata for a given job
-	def job_metadata
-		begin
-			@response[:data] = JSON.parse(@job.metadata)
-		rescue
-			error_msg(ErrorCodes::DATA_ACCESS_ERROR, "Could not get metadata for job '#{params[:job_id]}'")
-		end
-		render_json
-	end
+  # Returns the metadata for a given job
+  def job_metadata
+    begin
+      @response[:data] = JSON.parse(@job.metadata)
+    rescue
+      error_msg(ErrorCodes::DATA_ACCESS_ERROR, "Could not get metadata for job '#{params[:job_id]}'")
+    end
+    render_json
+  end
 
-	# Updates metadata for a specific key
-	def update_metadata
-		begin
-			@job.update_metadata_key(params[:key], params[:metadata])
-		rescue
-			error_msg(ErrorCodes::DATA_ACCESS_ERROR, "Could not update metadata for job '#{params[:job_id]}'")
-		end
-		render_json
-	end
+  # Updates metadata for a specific key
+  def update_metadata
+    begin
+      @job.update_metadata_key(params[:key], params[:metadata])
+    rescue
+      error_msg(ErrorCodes::DATA_ACCESS_ERROR, "Could not update metadata for job '#{params[:job_id]}'")
+    end
+    render_json
+  end
 
-	# Returns a job to work on for given process unless too many processes are already running
-	def process_request
-		job = nil
+  # Returns a job to work on for given process unless too many processes are already running
+  def process_request
+    job = nil
 
-		return unless process_startable #Return if process cannot start for some reason
+    return unless process_startable #Return if process cannot start for some reason
 
-		# Find job that is PENDING for method
-		job = @process.first_pending_job
+    # Find job that is PENDING for method
+    job = @process.first_pending_job
 
-		if job.nil?
-			error_msg(ErrorCodes::QUEUE_ERROR, "No jobs currently waiting for process with id '#{params[:process_id]}", @process.errors)
-		else
-			@response[:data] = {job_id: job.id, params: job.current_entry.flow_step.params}
-		end
-		render_json
-	end
+    if job.nil?
+      error_msg(ErrorCodes::QUEUE_ERROR, "No jobs currently waiting for process with id '#{params[:process_id]}", @process.errors)
+    else
+      @response[:data] = {job_id: job.id, params: job.current_entry.flow_step.params}
+    end
+    render_json
+  end
 
-	# Initiates given process for given job
-	def process_initiate
+  # Initiates given process for given job
+  def process_initiate
 
-	  return unless process_startable # Return if process cannot start for some reason
+    return unless process_startable # Return if process cannot start for some reason
 
-	  if !@process.update_state_for_job(@job.id, "STARTED")
-	  	error_msg(ErrorCodes::DATA_ACCESS_ERROR, "Could not change state for job with id '#{params[:job_id]}'", @process.errors)
-	  end
-	  render_json
-	end
+    if !@process.update_state_for_job(@job.id, "STARTED")
+      error_msg(ErrorCodes::DATA_ACCESS_ERROR, "Could not change state for job with id '#{params[:job_id]}'", @process.errors)
+    end
+    render_json
+  end
 
-	# Sets a process for given job and process_method as done
-	def process_done
-		if !@process.update_state_for_job(@job.id, "DONE")
-			error_msg(ErrorCodes::DATA_ACCESS_ERROR, "Could not change state for job with id '#{params[:job_id]}", @process.errors)
-		end
-		render_json
-	end
+  # Sets a process for given job and process_method as done
+  def process_done
+    if !@process.update_state_for_job(@job.id, "DONE")
+      error_msg(ErrorCodes::DATA_ACCESS_ERROR, "Could not change state for job with id '#{params[:job_id]}", @process.errors)
+    end
+    render_json
+  end
 
-	# Contains progress information about given job and process
-	def process_progress
-		if !@process.update_progress_for_job(@job.id, params[:progress_info])
-			error_msg(ErrorCodes::DATA_ACCESS_ERROR, "Could not update progress information for job with id '#{params[:job_id]}", @process.errors)
-		end
-		render_json
-	end
+  # Contains progress information about given job and process
+  def process_progress
+    if !@process.update_progress_for_job(@job.id, params[:progress_info])
+      error_msg(ErrorCodes::DATA_ACCESS_ERROR, "Could not update progress information for job with id '#{params[:job_id]}", @process.errors)
+    end
+    render_json
+  end
 
-	# Creates a job from given parameter data
-	def create_job
-		job_params = params[:data]
-		job_params[:metadata] = job_params[:metadata].to_json
-		parameters = ActionController::Parameters.new(job_params)
-		job = Job.create(parameters.permit(:name, :title, :author, :metadata, :xml, :source_id, :catalog_id, :comment, :object_info, :flow_id, :flow_params))
+  # Creates a job from given parameter data
+  def create
+    # logger.info "OrdersController#create: Begins"
+    # status = 500
+    # #my_data = params[:order].to_hash
+    # obj = Order.new(permitted_params)
 
-		if !job.save
-			error_msg(ErrorCodes::OBJECT_ERROR, "Could not save job with name '#{job[:name]}", job.errors)
-		end
-		render_json
-	end
+    # if obj.save!
+    #   logger.info "OrdersController#update: Object successfully saved."
+    #   headers['location'] = "/orders/#{obj.id}"
+    #   #headers['location'] = "#{:order_url}"
+    #   status = 201
+    #   logger.info "==== Here is Header Info ==================="
+    #   logger.info "#{headers['location']}"
+    #   logger.info "============================================"
+    #   logger.info "==== Here is json for the created object ==="
+    #   logger.info "#{obj.as_json}"
+    #   logger.info "============================================"
+    pp "==========================="
+    pp params
+    pp "==========================="
+    job_params = params['job']
+    job_params[:metadata] = job_params[:metadata].to_json
+    parameters = ActionController::Parameters.new(job_params)
+    pp "-+-+-+-+-+-+-+-+"
+    pp parameters
+    pp "-+-+-+-+-+-+-+-+"
+    #job = Job.new()
+    job = Job.create(parameters.permit(:name, :title, :author, :metadata, :xml, :source, :catalog_id, :comment, :object_info, :flow_id, :flow_params, :treenode_id))
 
-	# Checks if job exists, and sets @job variable. Otherwise, return error.
-	private
-	def check_params
-		return unless check_job_id
-		return unless check_process_id
-		return unless check_job_and_process
-	end
+    if !job.save
+      #error_msg(ErrorCodes::OBJECT_ERROR, "Could not save job with name '#{job[:name]}", job.errors)
+      error_msg(ErrorCodes::OBJECT_ERROR, "Could not save job...", job.errors)
+    end
+    render_json
+  rescue Exception => e
+    #pp "Error in fetch_from_libris #{e.message}"
+    pp "A terrible error occurred: '#{e.message}'.    Please try harder!"
+    #error_msg(ErrorCodes::OBJECT_ERROR, "Could not save job with name '#{job[:name]}", job.errors)
+    error_msg(ErrorCodes::OBJECT_ERROR, "Could not save job...")
+    render_json
+  end
 
-	#Check job_id
-	def check_job_id
-		if params[:job_id]
-			@job = Job.where(id: params[:job_id]).first
-			if @job.nil?
-				error_msg(ErrorCodes::OBJECT_ERROR, "Could not find job '#{params[:job_id]}'")
-				render_json
-				return false
-			end
-		end
-		return true
-	end
+  # Checks if job exists, and sets @job variable. Otherwise, return error.
+  private
+  def check_params
+    return unless check_job_id
+    return unless check_process_id
+    return unless check_job_and_process
+  end
 
-	#Check process_id
-	def check_process_id
-		if params[:process_code]
-			@process = ProcessModel.find_on_code(params[:process_code])
-			if !@process
-				error_msg(ErrorCodes::OBJECT_ERROR, "Could not find process with id '#{params[:process_id]}'")
-				render_json
-				return false
-			end
-		end
-		return true
-	end
+  #Check job_id
+  def check_job_id
+    if params[:job_id]
+      @job = Job.where(id: params[:job_id]).first
+      if @job.nil?
+        error_msg(ErrorCodes::OBJECT_ERROR, "Could not find job '#{params[:job_id]}'")
+        render_json
+        return false
+      end
+    end
+    return true
+  end
 
-	#If both job and process are set, check if they are valid together
-	def check_job_and_process
-		if params[:job_id] && params[:process_code]
-			if @job.current_entry.flow_step.process_id != @process.id
-				error_msg(ErrorCodes::QUEUE_ERROR, "Job with id '#{params[:process_code]}' is not currently working on #{params[:process_code]}")
-				render_json
-				return false
-			end
-		end
-		return true
-	end
+  #Check process_id
+  def check_process_id
+    if params[:process_code]
+      @process = ProcessModel.find_on_code(params[:process_code])
+      if !@process
+        error_msg(ErrorCodes::OBJECT_ERROR, "Could not find process with id '#{params[:process_id]}'")
+        render_json
+        return false
+      end
+    end
+    return true
+  end
 
-	# Checks if process is startable and sets proper error messages if not
-	def process_startable
-		# Check if the allowed amount of processes are already running
-		if !@process.startable?
-			error_msg(ErrorCodes::QUEUE_ERROR, "Too many running processes with id '#{params[:process_id]}", @process.errors)
-			render_json
-			return false
-		end
-		return true
-	end
+  #If both job and process are set, check if they are valid together
+  def check_job_and_process
+    if params[:job_id] && params[:process_code]
+      if @job.current_entry.flow_step.process_id != @process.id
+        error_msg(ErrorCodes::QUEUE_ERROR, "Job with id '#{params[:process_code]}' is not currently working on #{params[:process_code]}")
+        render_json
+        return false
+      end
+    end
+    return true
+  end
+
+  # Checks if process is startable and sets proper error messages if not
+  def process_startable
+    # Check if the allowed amount of processes are already running
+    if !@process.startable?
+      error_msg(ErrorCodes::QUEUE_ERROR, "Too many running processes with id '#{params[:process_id]}", @process.errors)
+      render_json
+      return false
+    end
+    return true
+  end
 
 end
