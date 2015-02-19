2a1623f5feab1d739ec8daa729b02beaf8ba8810
Jimmy Carlsson <jimmy.carlsson@ub.gu.se>
2015-02-19 10:19:01 +0100

    Fixed migration

--- db/migrate/20140808113408_create_sources.rb
+++ db/migrate/20140808113408_create_sources.rb
@@ -1,12 +1,12 @@
 class CreateSources < ActiveRecord::Migration
 	def change
-		create_table :sources do |t|
-			t.text     "classname"
-			t.timestamps
-		end
-		Source.reset_column_information
-		Source.create(classname: "Libris")
-		Source.create(classname: "Letter")
-		Source.create(classname: "Document")
+		# create_table :sources do |t|
+		# 	t.text     "classname"
+		# 	t.timestamps
+		# end
+		# Source.reset_column_information
+		# Source.create(classname: "Libris")
+		# Source.create(classname: "Letter")
+		# Source.create(classname: "Document")
 	end
 end
