cb04b680104e3fd7be5704a0b1aa1940ed4bfd3d
Jimmy Carlsson <jimmy.carlsson@ub.gu.se>
2015-02-19 13:56:12 +0100

    Fix to be able to migrate empty db

--- config/initializers/create_local_users.rb
+++ config/initializers/create_local_users.rb
@@ -1,4 +1,4 @@
 # Read all users from passwd file and create users that do not already exist
-if Rails.env != "test"
+if Rails.env != "test" && (ActiveRecord::Base.connection.table_exists? 'users') # Checks if table exists to be able to migrate a new db
   User.create_missing_users_from_file("#{Rails.root}/config/passwd")
 end
