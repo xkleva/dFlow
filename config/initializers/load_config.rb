b9cc81a4e8e2b99d79645984872732f673451151
Jimmy Carlsson <jimmy.carlsson@ub.gu.se>
2015-03-06 10:33:04 +0100

    Add secret sample file

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,1 +1,3 @@
-APP_CONFIG = YAML.load_file("#{Rails.root}/config/config.yml")
+main_config = YAML.load_file("#{Rails.root}/config/config.yml")
+secret_config = YAML.load_file("#{Rails.root}/config/config_secret.yml")
+APP_CONFIG = main_config.merge(secret_config)
