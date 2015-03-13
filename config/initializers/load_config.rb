efbf3db7e0e653f0b254749af327caa00cff776a
Jimmy Carlsson <jimmy.carlsson@ub.gu.se>
2015-03-13 17:01:27 +0100

    Moved user load to make sure they load after APP_CONFIG is set

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,3 +1,4 @@
+# Read config files and store applicable values in APP_CONFIG constant
 main_config = YAML.load_file("#{Rails.root}/config/config.yml")
 if Rails.env == 'test' || Rails.env == 'development'
   secret_config = YAML.load_file("#{Rails.root}/config/config_secret.test.yml")
@@ -5,3 +6,8 @@ else
   secret_config = YAML.load_file("#{Rails.root}/config/config_secret.yml")
 end
 APP_CONFIG = main_config.merge(secret_config)
+
+# Read all users from passwd file and create users that do not already exist
+if Rails.env != "test" && (ActiveRecord::Base.connection.table_exists? 'users') # Checks if table exists to be able to migrate a new db
+  User.create_missing_users_from_file("#{Rails.root}/config/passwd")
+end
