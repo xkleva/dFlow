dfbc27e399e7cb18c782aac6aea27627fb51b9ec
Johan Andersson von Geijer <Johan.Andersson.von.Geijer@ub.gu.se>
2015-04-28 08:09:42 +0200

    DFLOW-19 Added attribute accessors to DublinCore class.

--- app/models/sources/dublin_core.rb
+++ app/models/sources/dublin_core.rb
@@ -11,7 +11,40 @@ class DublinCore < Source
   # There is no schema for libris right now
   XML_SCHEMA = nil
   VALID_KEYS = ["source_name", "source_id", "catalog_id", "name", "metadata", "title", "author", "object_info", "comment", "flow_id", "flow_params"]
-  DC_KEYS = ["creator", "creator", "subject", "description", "publisher", "contributor", "date", "type", "format", "identifier", "source", "language", "relation", "coverage", "rights"]
+  DC_KEYS = [
+    "title",
+    "creator",
+    "subject",
+    "description",
+    "publisher",
+    "contributor",
+    "date",
+    "type",
+    "format",
+    "identifier",
+    "source",
+    "language",
+    "relation",
+    "coverage",
+    "rights"
+  ]
+
+  attr_accessor :catalog_id
+  attr_accessor :dc_title
+  attr_accessor :dc_creator
+  attr_accessor :dc_subject
+  attr_accessor :dc_description
+  attr_accessor :dc_publisher
+  attr_accessor :dc_contributor
+  attr_accessor :dc_date
+  attr_accessor :dc_type
+  attr_accessor :dc_format
+  attr_accessor :dc_identifier
+  attr_accessor :dc_source
+  attr_accessor :dc_language
+  attr_accessor :dc_relation
+  attr_accessor :dc_coverage
+  attr_accessor :dc_rights
 
   # Validates fields of job object
   def self.validate_job_fields(object)
@@ -22,28 +55,32 @@ class DublinCore < Source
   end
 
   # Returns a hash of data fetched from source
-  def self.fetch_source_data(dc_hash)
+  def self.fetch_source_data(catalog_id, dc_data={})
     job_data = {}
-    job_data = populate_with_dc(dc_hash)
+    job_data = populate_with_dc(catalog_id, dc_data)
     #job_data[:catalog_id] = catalog_id if not job_data.blank?
+    job_data[:catalog_id] = generate_catalog_id()
     return job_data
   end
 
   private
 
-  def self.populate_with_dc(dc_hash)
+  def self.populate_with_dc(catalog_id, dc_data={})
     job_data = {}
     # TODO pupulate job with DC data
     job_data = data_from_dc(dc_data)
     return job_data
   end
 
+  # dc_data is a hash with keys corresponding to the dc terms.
+  # Since each dc_term might have multiple values each dc_term
+  # has the values in an array.
   def self.data_from_dc(dc_data)
     job_data = {}
     if (dc_data)
-      job_data[:title] = ""
-      job_data[:author] = ""
-      job_data[:metadata] = {}
+      job_data[:title] = dc_data[:dc_title] # ? dc_data[:dc_title][0] : ""
+      job_data[:author] = dc_data[:dc_creator] # ? dc_data[:dc_creator][0] : ""
+      job_data[:metadata] = dc_data
       job_data[:metadata][:type_of_record] =  ""
       job_data[:source_name] = Source.find_name_by_class_name(self.name)
       return job_data
@@ -51,6 +88,10 @@ class DublinCore < Source
       pp "No valid record"
       return {}
     end
+  end
+
+  def self.generate_catalog_id()
+    "dc:#{SecureRandom.uuid}"
   end
 
 end
