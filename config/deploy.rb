fdfae010bf8a87636d1a47fc3c1a17602267e5f0
jkcarlsson85 <jkcarlsson85@gmail.com>
2016-02-03 17:31:39 +0100

    Make configuration file loading more dynamic allowing any environment prefix for files, instead of just development and test. Also fix bug which made the test suite to create a valid configuration file for other environments.

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,9 +2,10 @@
 lock '3.4.0'
 
 set :application, 'dFlow'
-#set :repo_url, 'git@github.com:ub-digit/dFlow.git'
 set :repo_url, 'https://github.com/ub-digit/dFlow.git'
 
+# Copied into /{app}/shared/config from respective sample file
+set :linked_files, %w{config/database.yml config/config_full.yml config/passwd}
 set :rvm_ruby_version, '2.1.5'      # Defaults to: 'default'
 
 # Returns config for current stage assigned in config/deploy.yml
@@ -18,65 +19,4 @@ server deploy_config['host'], user: deploy_config['user'], roles: deploy_config[
 
 set :deploy_to, deploy_config['path']
 
-#set :rvm_ruby_string, :local              # use the same ruby as used locally for deployment
-#set :rvm_autolibs_flag, "read-only"       # more info: rvm help autolibs
 
-#before 'deploy:setup', 'rvm:install_rvm'  # install/update RVM
-#before 'deploy:setup', 'rvm:install_ruby' # install Ruby and create gemset, OR:
-# before 'deploy:setup', 'rvm:create_gemset' # only create gemset
-
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
-
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, '/var/www/my_app_name'
-
-# Default value for :scm is :git
-# set :scm, :git
-
-# Default value for :format is :pretty
-# set :format, :pretty
-
-# Default value for :log_level is :debug
-# set :log_level, :debug
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/secrets.yml')
-
-# Default value for linked_dirs is []
-# set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system')
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-#set :tmp_dir, '/home/rails/tmp'
-
-# Copied into /{app}/shared/config from respective sample file
-set :linked_files, %w{config/database.yml config/config_full.yml config/passwd}
-
-namespace :deploy do
-  namespace :assets do
-    task :precompile do
-      
-    end
-  end
-end
-
-namespace :deploy do
-
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
-
-end
--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -15,6 +15,10 @@ end
 
 class ConfigLoader
 
+  def self.environments
+    @@environments ||= Dir.glob("./config/environments/*.rb").map { |filename| File.basename(filename, ".rb") }
+  end
+
   def self.load_config_structure(base_file_path:)
 
     base_file = Pathname.new(base_file_path)
@@ -31,15 +35,15 @@ class ConfigLoader
         main_config = load_config_file(path: file, config_hash: main_config)
       end
 
-      # Load test files if currently in test environment
-      if Rails.env == 'test'
-        files = d_folder.children.sort_by { |x| x.basename.to_s[/^(\d+)\./,1].to_i }.reverse
-        files.each do |file|
-          if file.basename.to_s.downcase.start_with?('test')
-            main_config = load_config_file(path: file, config_hash: main_config, test_file: true)
-          end
+      # Load files specific to current environment (eg test, development), separated to make sure environment files overwrite generic configuration files
+        
+      files = d_folder.children.sort_by { |x| x.basename.to_s[/^(\d+)\./,1].to_i }.reverse
+      files.each do |file|
+        if file.basename.to_s.downcase.start_with?(Rails.env)
+          main_config = load_config_file(path: file, config_hash: main_config, environment_file: true)
         end
       end
+
     else
       Rails.logger.info "There is no app-config-d directory, aborting"
     end
@@ -48,7 +52,7 @@ class ConfigLoader
 
   end
 
-  def self.load_config_file(path:, config_hash:, test_file: false)
+  def self.load_config_file(path:, config_hash:,environment_file: false)
 
     # Check if file exists
     if !path.exist?
@@ -62,15 +66,15 @@ class ConfigLoader
       return config_hash
     end
 
-    # Check if file is a test config
-    if test_file
-      if (!path.basename.to_s.downcase.start_with?('test'))
-        Rails.logger.debug "Not loading #{path.to_s} as it is not a configuration file for test"
+    # Check if file is an environment specific config
+    if environment_file
+      if (!path.basename.to_s.downcase.start_with?(Rails.env))
+        Rails.logger.debug "Not loading #{path.to_s} as it is not a configuration file for #{Rails env}"
         return config_hash
       end
     else
-      if (path.basename.to_s.downcase.start_with?('test'))
-        Rails.logger.debug "Not loading #{path.to_s} as it is a configuration file for test"
+      if ConfigLoader.environments.any? {|environment| (path.basename.to_s.downcase.start_with?(environment))}
+        Rails.logger.debug "Not loading #{path.to_s} as it is a configuration file for #{path.basename.to_s.downcase}"
         return config_hash
       end
     end
@@ -100,7 +104,8 @@ class ConfigLoader
   # Generates a combined config/config_full.yml file containing all config data
   def self.generate_file(base_file_path:)
     app_config = load_config_structure(base_file_path: base_file_path)
-    config_file = File.open("#{Rails.root}/config/config_full.yml", "w:utf-8") do |file|
+    filename = "config_full"
+    config_file = File.open("#{Rails.root}/config/config_full_#{Rails.env}.yml", "w:utf-8") do |file|
       file.write(app_config.to_yaml)
     end
   end
@@ -108,9 +113,9 @@ class ConfigLoader
 end
 
 # If in test environment, generate file. Otherwise it should already exist.
-if Rails.env == 'test'
+if Rails.env == 'test' || Rails.env == 'development'
   ConfigLoader.generate_file(base_file_path: "#{Rails.root}/config/app-config.yml")
 end
-main_config = YAML.load_file("#{Rails.root}/config/config_full.yml")
+main_config = YAML.load_file("#{Rails.root}/config/config_full_#{Rails.env}.yml")
 
 APP_CONFIG = main_config
