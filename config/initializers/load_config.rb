ecf5a264b109e3b13a855ed774a6de6b5c666817
jkcarlsson85 <jkcarlsson85@gmail.com>
2016-02-24 16:41:14 +0100

    Move APP_CONFIG dependent code to after it has been initialized

--- config/initializers/load_config.rb
+++ config/initializers/load_config.rb
@@ -1,7 +1,3 @@
-# Read all users from passwd file and create users that do not already exist
-if Rails.env != "test" && (ActiveRecord::Base.connection.table_exists? 'users') # Checks if table exists to be able to migrate a new db
-  User.create_missing_users_from_file("#{Rails.root}/config/passwd")
-end
 
 # If in test environment, generate file. Otherwise it should already exist.
 if Rails.env == 'test' || Rails.env == 'development'
@@ -11,3 +7,8 @@ end
 main_config = YAML.load_file("#{Rails.root}/config/config_full_#{Rails.env}.yml")
 
 APP_CONFIG = main_config
+
+# Read all users from passwd file and create users that do not already exist
+if Rails.env != "test" && (ActiveRecord::Base.connection.table_exists? 'users') # Checks if table exists to be able to migrate a new db
+  User.create_missing_users_from_file("#{Rails.root}/config/passwd")
+end
