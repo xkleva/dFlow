750739fcb3eb41f1a6d42a0e306fceb737f1f506
jkcarlsson85 <jkcarlsson85@gmail.com>
2016-07-05 11:30:55 +0200

    Fix small bug where format_params was required for process COPY_FOLDER

--- app/models/processes/copy_folder/copy_folder.rb
+++ app/models/processes/copy_folder/copy_folder.rb
@@ -1,6 +1,6 @@
 class CopyFolder
 
-  def self.run(job:, logger: QueueManager.logger, source_folder_path:, destination_folder_path:, format_params:)
+  def self.run(job:, logger: QueueManager.logger, source_folder_path:, destination_folder_path:, format_params: '')
 
     if DfileApi.copy_folder_ind(source_dir: source_folder_path, dest_dir: destination_folder_path, flow_step: job.flow_step, format_params: format_params)
       return true
