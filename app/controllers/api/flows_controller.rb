dc83c1815d145bbff6767bfcbc06edf594c10d47
jkcarlsson85 <jkcarlsson85@gmail.com>
2016-08-08 17:05:13 +0200

    INitialize json arrays to be empty arrays if empty or nil when reaching api callbakc

--- app/controllers/api/flows_controller.rb
+++ app/controllers/api/flows_controller.rb
@@ -29,14 +29,15 @@ class Api::FlowsController < Api::ApiController
       error_msg(ErrorCodes::OBJECT_ERROR, "Could not find flow with id '#{params[:id]}'")
     end
 
-    flow_params[:steps] = flow_params[:flow_steps]['flow_steps'].to_json
-    flow_params[:parameters] = flow_params[:parameters]['parameters'].to_json
-    flow_params[:folder_paths] = flow_params[:folder_paths]['folder_paths'].to_json
+    pp flow_params[:folder_paths]
+    flow_params[:steps] = flow_params[:flow_steps]['flow_steps'].present? ? flow_params[:flow_steps]['flow_steps'].to_json : '[]'
+    flow_params[:parameters] = flow_params[:parameters]['parameters'].present? ? flow_params[:parameters]['parameters'].to_json : '[]'
+    flow_params[:folder_paths] = flow_params[:folder_paths]['folder_paths'].present? ? flow_params[:folder_paths]['folder_paths'].to_json : '[]'
 
     if flow.update_attributes(permitted_params)
       @response[:flow] = flow
     else
-      error_msg(ErrorCodes::VALIDATION_ERROR, "Could not save flow #{params[:id]}")
+      error_msg(ErrorCodes::VALIDATION_ERROR, "Could not save flow #{params[:id]}", flow.errors)
     end
     render_json
   end
@@ -48,7 +49,7 @@ class Api::FlowsController < Api::ApiController
     if flow.save
       @response[:flow] = flow
     else
-      error_msg(ErrorCodes::VALIDATION_ERROR, "Could not save flow #{params[:id]}")
+      error_msg(ErrorCodes::VALIDATION_ERROR, "Could not save flow #{params[:id]}", flow.errors)
     end
     render_json
   end
