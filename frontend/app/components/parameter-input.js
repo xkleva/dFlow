84ab28bcf757750afc2198ad5e3a364a0dbd2744
Stefan Berndtsson <stefan.berndtsson@gmail.com>
2016-08-18 14:16:05 +0200

    Enable use of labels for options in flow parameters, and a text type as well

--- frontend/app/components/parameter-input.js
+++ frontend/app/components/parameter-input.js
@@ -1,14 +1,28 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
+  prompt: true,
   initValue: function(){
     this.set('value', this.get('values.' + this.get('parameter.name')));
   }.on('init'),
 
   isRadio: Ember.computed.equal('parameter.type', 'radio'),
+  isText: Ember.computed.equal('parameter.type', 'text'),
 
   valueObserver: Ember.observer('value', function(){
     this.set('values.' + this.get('parameter.name'), this.get('value'));
+  }),
+
+  optionList: Ember.computed('parameter.options', function() {
+    return this.get('parameter.options').map(function(option) {
+      if(typeof(option) === "string") {
+        return {
+          value: option,
+          label: option
+        }
+      } else {
+        return option;
+      }
+    });
   })
 });
